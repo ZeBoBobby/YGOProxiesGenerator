ac28849f9cc8346c7d53620a8b1ffa05629d5bf2
BoBobby <mazoyer.alexis@gmail.com>
2020-11-21 14:00:36 +0100

    Switch to web app

--- index.js
+++ index.js
@@ -1,12 +1,70 @@
-PDFDocument = require('pdfkit');
-fs = require('fs');
-Path = require('path');
-Axios = require('axios');
-doc = new PDFDocument({
+const PDFDocument = require('pdfkit');
+const express = require('express')
+const app = express()
+const fs = require('fs');
+const Path = require('path');
+const Axios = require('axios');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+
+const doc = new PDFDocument({
     size: 'A4',
     margin: 0
 });
+let decklist;
 
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+app.set('view engine', 'ejs');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+app.post('/upload-decklist', async (req, res) => {
+    console.log(req.files.deckfile);
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No deckfile uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "deckfile") to retrieve the uploaded file
+            let deckfile = req.files.deckfile;
+            
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            let fileLocation = './upload_ydk/' + deckfile.name;
+            deckfile.mv(fileLocation).then(a => {
+                downloadImages(fileLocation).then(e => {
+                    makePdfProxies();
+                    console.log("All done!");
+                    res.render('pages/success');
+                }).catch(error => console.error(error));
+            })
+
+            //send response
+            // res.send({
+            //     status: true,
+            //     message: 'File is uploaded' + fileLocation,
+            //     data: {
+            //         name: deckfile.name,
+            //         mimetype: deckfile.mimetype,
+            //         size: deckfile.size
+            //     }
+            // });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 //Permet de récupérer des images de cartes
 async function downloadImage(prmIdCard) {
@@ -28,9 +86,15 @@ async function downloadImage(prmIdCard) {
         })
 }
 
-//console.log(fs.existsSync('./images/10045474.jpg'));
+const onlyCardsId = prmString => {
+    if ((prmString.includes('#') || prmString.includes('!')) || prmString.length < 4) {
+        return false;
+    } 
+    return true;
+}
 
-decklist = require('./deck.json');
+// var decklist = require('./deck.json');
+
 const removeFirstZero = e => {
     if(e.charAt(0) !== "0") {
         return e;
@@ -38,7 +102,15 @@ const removeFirstZero = e => {
         return removeFirstZero(e.slice(1));
     }
 }
-const downloadImages = async _ => {
+const downloadImages = async prmDeckfile => {
+    console.log("New images downloading...");
+    console.log('from : ', prmDeckfile);   // var decklist;
+    // await fs.readFile('./decklist.ydk', function(err, data) {
+    //     if(err) throw err;
+    //     decklist = data.toString().split("\n").filter(elm => onlyCardsId(elm));
+    // });
+    decklist = fs.readFileSync(prmDeckfile).toString().split("\n").filter(elm => onlyCardsId(elm)).map(elm => elm.trim());
+    console.log(decklist);
 
     for (let i = 0; i < decklist.length; i++) {
         decklist[i] = removeFirstZero(decklist[i]);
@@ -63,6 +135,7 @@ const makePdfProxies = async _ => {
         let height = 252;
         let margin = 0;
 
+        console.log("PDF file creation...");
         for (let i = 0; i < decklist.length; i++) {
             // console.log("i : " + i);
             // console.log("x : " + x);
@@ -100,6 +173,15 @@ const makePdfProxies = async _ => {
 }
 
 
-downloadImages().then(e => {
-    makePdfProxies();
-}).catch(error => console.error(error));
+// downloadImages().then(e => {
+//     makePdfProxies();
+//     console.log("All done!");
+// }).catch(error => console.error(error));
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+  })
+  
+  app.listen(8080, () => {
+    console.log(`Example app listening at http://localhost:8080`)
+  })
