b7fd23d07bbc12e66b5e6be0ec35248629000bd6
bobby <mazoyer.alexis@gmail.com>
2024-01-22 23:07:28 +0100

    add ydke input option clear old code throw errors

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const PDFDocument = require('pdfkit');
+const ydke = require("ydke");
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const Path = require('path');
-const Axios = require('axios');
+const path = require('path');
+const axios = require('axios');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
-const pdfFolder = `${__dirname}/pdf`;
+const pdfFolder = path.join(__dirname, 'pdf');
+const uploadFolder = path.join(__dirname, 'upload_ydk');
+const imagesFolder = path.join(__dirname, 'images');
 
 const port = 8080;
 
-const makeId = prmLength => {
+const makeId = (prmLength) => {
   let result = '';
-  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charsLength = chars.length;
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charsLength = chars.length;
   for (let i = 0; i < prmLength; i++) {
-    result += chars.charAt(Math.floor(Math.random() * charsLength))
+    result += chars.charAt(Math.floor(Math.random() * charsLength));
   }
   return result;
-}
+};
 
 let decklist;
 
@@ -29,11 +32,11 @@ let decklist;
 app.use(fileUpload({
   createParentPath: true,
   limits: {
-    fileSize: 2 * 1024 * 1024 * 1024 //2MB max file(s) size
-  }
+    fileSize: 2 * 1024 * 1024 * 1024, // 2MB max file(s) size
+  },
 }));
 
-app.use(favicon(Path.join(__dirname, '.', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, '.', 'favicon.ico')));
 
 app.disable('etag');
 
@@ -44,169 +47,177 @@ app.set('view engine', 'ejs');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
 app.use(morgan('dev'));
 
+// Middleware to handle errors consistently
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render('pages/success', {
+    success: '',
+    page: '',
+    error: 'Internal Server Error',
+    lang: req.acceptsLanguages()[0],
+  });
+});
+
+const isYdkFile = (filename) => {
+  return filename.endsWith('.ydk');
+};
+
+const flattenObjectValues = (obj) => {
+  return Object.values(obj).reduce((acc, currentValue) => {
+    return acc.concat(Array.from(currentValue).map(item => item.toString()));
+  }, []);
+};
+const generateUniqueFilename = (originalFilename) => {
+  const prefix = originalFilename.replace(/\.[^/.]+$/, '').substring(0, 12).replace(/\s+/g, '').replace(/[^\w\s]/gi, '');
+  return `${prefix}_${makeId(12)}.pdf`;
+};
+
 app.post('/upload-decklist', async (req, res) => {
   try {
-    if (!req.files) {
-      res.render('pages/success', {
-        success: '',
-        page: '',
-        error: 'Uploaded file is empty or corrupted',
-        lang: req.acceptsLanguages()[0]
-      });
-    } else {
-      //Use the name of the input field (i.e. "deckfile") to retrieve the uploaded file
-      let deckfile = req.files.deckfile;
+    if (!req.files && !req.body.ydkeCode) {
+      throw new Error('No uploaded file or YDKE code provided.');
+    }
 
-      //Use the mv() method to place the file in upload directory (i.e. "uploads")
-      let fileLocation = './upload_ydk/' + deckfile.name;
-      deckfile.mv(fileLocation).then(a => {
-        downloadImages(fileLocation).then(e => {
-          const filename = deckfile.name.replace(/\.[^/.]+$/, "").substring(0.12).replace(/\s+/g, '').replace(/[^\w\s]/gi, '') + makeId(12) + '.pdf';
-          makePdfProxies(filename, fileLocation);
-          console.log("All done!");
-          res.render('pages/success', {
-            filename: filename,
-            success: 'success',
-            page: 'upload',
-            error: '',
-            lang: req.acceptsLanguages()[0]
-          });
-        }).catch(error => {
-          console.error(error)
-          fs.unlinkSync(fileLocation);
-          res.render('pages/success', {
-            success: '',
-            page: '',
-            error: error,
-            lang: req.acceptsLanguages()[0]
-          });
-        });
-      })
+    if (req.files && req.files.deckfile) {
+      const deckfile = req.files.deckfile;
+      if (!isYdkFile(deckfile.name)) {
+        throw new Error('Invalid file type. Only .ydk files are allowed.');
+      }
+
+      const fileLocation = path.join(uploadFolder, deckfile.name);
+      await deckfile.mv(fileLocation);
+
+      const filename = generateUniqueFilename(deckfile.name);
+      decklist = getYdkDecklist(fileLocation);
+      await downloadImages(decklist);
+      makePdfProxies(filename);
+
+      res.render('pages/success', {
+        filename,
+        success: 'success',
+        page: 'upload',
+        error: '',
+        lang: req.acceptsLanguages()[0],
+      });
+    } else if (req.body.ydkeCode) {
+      const ydkeCode = req.body.ydkeCode;
+      const sanitizedCode = !ydkeCode.startsWith("ydke://") ? "ydke://"+ydkeCode : ydkeCode;
+      const decodedCode = ydke.parseURL(sanitizedCode);
+      decklist = flattenObjectValues(decodedCode);
+
+      const filename = generateUniqueFilename('ydke_deck.pdf');
+      await downloadImages(decklist);
+      makePdfProxies(filename);
+
+      res.render('pages/success', {
+        filename,
+        success: 'success',
+        page: 'upload',
+        error: '',
+        lang: req.acceptsLanguages()[0],
+      });
     }
   } catch (err) {
-    fs.unlinkSync(fileLocation);
-    res.status(500).render('pages/success', {
+    console.error(err);
+    res.render('pages/success', {
       success: '',
       page: '',
-      error: err,
-      lang: req.acceptsLanguages()[0]
+      error: err.message,
+      lang: req.acceptsLanguages()[0],
     });
   }
 });
 
-//Permet de récupérer des images de cartes
-const downloadImage = async prmIdCard => {
-  const url = 'https://images.ygoprodeck.com/images/cards/' + prmIdCard + '.jpg';
-  const path = Path.resolve(__dirname, 'images', prmIdCard + '.jpg');
-  const writer = fs.createWriteStream(path);
+const downloadImage = async (prmIdCard) => {
+  const url = `https://images.ygoprodeck.com/images/cards/${prmIdCard}.jpg`;
+  const imagePath = path.join(imagesFolder, `${prmIdCard}.jpg`);
+  const writer = fs.createWriteStream(imagePath);
 
-  const response = await Axios({
+  const response = await axios({
     url,
     method: 'GET',
-    responseType: 'stream'
-  })
+    responseType: 'stream',
+  });
 
-  response.data.pipe(writer)
+  response.data.pipe(writer);
 
   return new Promise((resolve, reject) => {
-    writer.on('finish', resolve)
-    writer.on('error', reject)
-  })
-}
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
+};
 
-const onlyCardsId = prmString => {
-  if ((prmString.includes('#') || prmString.includes('!')) || prmString.length < 4 ) {
-    return false;
-  }
-  return true;
-}
+const onlyCardsId = (prmString) => {
+  return !(prmString.includes('#') || prmString.includes('!') || prmString.length < 4);
+};
 
-const removeFirstZero = e => {
-  if (e.charAt(0) !== "0") {
-    return e;
-  } else {
-    return removeFirstZero(e.slice(1));
-  }
-}
+const removeFirstZero = (e) => {
+  return e.charAt(0) !== '0' ? e : removeFirstZero(e.slice(1));
+};
 
-const downloadImages = async (prmDeckfile) => {
-  console.log("New images downloading...");
-  console.log('from : ', prmDeckfile); // var decklist;
-
-  decklist = fs.readFileSync(prmDeckfile).toString().split("\n").filter(elm => onlyCardsId(elm)).map(elm => elm.trim());
+const getYdkDecklist = (deckfilePath) => {
+  return fs.readFileSync(deckfilePath).toString().split('\n').filter((elm) => onlyCardsId(elm)).map((elm) => elm.trim());
+};
+const downloadImages = async (decklist) => {
+  console.log('New images downloading...');
 
   for (let i = 0; i < decklist.length; i++) {
     decklist[i] = removeFirstZero(decklist[i]);
 
-    const path = './images/' + decklist[i] + '.jpg';
-    if (!fs.existsSync(path)) {
+    const imagePath = path.join(imagesFolder, `${decklist[i]}.jpg`);
+    if (!fs.existsSync(imagePath)) {
       await downloadImage(decklist[i]);
     }
   }
+};
 
-}
-
-const makePdfProxies = async (prmFileName, prmYDKFileToDelete) => {
-
+const makePdfProxies = async (prmFileName) => {
   const doc = new PDFDocument({
     size: 'A4',
     margin: 0,
     permissions: {
-      printing: 'highResolution'
+      printing: 'highResolution',
     },
     info: {
       Title: 'Proxy file',
-      Author: 'YGOProxy.com'
-    }
+      Author: 'YGOProxy.com',
+    },
   });
-  //Pipe its output somewhere, like to a file or HTTP response 
-  //See below for browser usage 
-  doc.pipe(fs.createWriteStream(pdfFolder + '/' + prmFileName))
+
+  doc.pipe(fs.createWriteStream(path.join(pdfFolder, prmFileName)));
 
   let x = 40;
   let y = 40;
-  let width = 167;
-  let height = 244;
-  // let width = 173;
-  // let height = 252;
-  let margin = 0;
+  const width = 167;
+  const height = 244;
+  const margin = 0;
 
-  console.log("PDF file creation...");
+  console.log('PDF file creation...');
   for (let i = 0; i < decklist.length; i++) {
-
-    const path = './images/' + decklist[i] + '.jpg';
+    const imagePath = path.join(imagesFolder, `${decklist[i]}.jpg`);
 
     if (i % 3 !== 0) {
       x = x + width + margin;
-      //y = y + height;   
     } else if (i !== 0) {
       x = 40;
       y = y + height + margin;
     }
+
     if (y + height > 842) {
       y = 40;
       x = 40;
-      doc.addPage().image(path, x, y, {
-        fit: [width, height],
-        //    align: 'center',
-        //    valign: 'center'
-      });
+      doc.addPage().image(imagePath, x, y, { fit: [width, height] });
     } else {
-      doc.image(path, x, y, {
-        fit: [width, height],
-        //    align: 'center',
-        //    valign: 'center'
-      });
+      doc.image(imagePath, x, y, { fit: [width, height] });
     }
   }
-  //Add an image, constrain it to a given size, and center it vertically and horizontally 
+
   doc.end();
-  fs.unlinkSync(prmYDKFileToDelete);
-}
+};
 
 app.get('/', (req, res) => {
   res.render('pages/index', {
